3e9238a9320d9a5361661a63f776ab77baad0d74
quotingg. <112746878+quotingg@users.noreply.github.com>
2025-02-10 19:33:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const axios = require("axios");
+const path = require("node:path");
+const app = express();
+
+// static stuff
+app.use("/3", express.static(path.join(__dirname, "3")));
+app.use("/apps", express.static(path.join(__dirname, "apps")));
+app.use("/play", express.static(path.join(__dirname, "play")));
+
+// parse
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+///apps/uncube/10005
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "apps/uncube/10005", "now.html"));
+})
+
+app.get("/oapi/3/np/v1/playPass", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/oapi/3/np/v1/playPass",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.post("/oapi/3/play/v1/reportEvent", (req, res) => {
+    axios({
+        method: 'post',
+        url: "https://now.gg/oapi/3/play/v1/reportEvent",
+        data: req.body
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.get("/oapi/3/play/v1/getQuestInfo", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/oapi/3/play/v1/getQuestInfo",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.get("/oapi/3/play/v1/getAppPlayFeatures", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/oapi/3/play/v1/getAppPlayFeatures",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.get("/oapi/3/play/v1/startSession", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/oapi/3/play/v1/startSession",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.post("/oapi/3/play/v1/createPlayUser", (req, res) => {
+    axios({
+        method: 'post',
+        url: "https://now.gg/oapi/3/play/v1/createPlayUser",
+        data: req.body
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.post("/oapi/3/play/v1/setAnswerSdp", (req, res) => {
+    axios({
+        method: 'post',
+        url: "https://now.gg/oapi/3/play/v1/setAnswerSdp",
+        data: req.body
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.post("/accounts/auth/v2/access-token", (req, res) => {
+    axios({
+        method: 'post',
+        url: "https://now.gg/accounts/auth/v2/access-token",
+        data: req.body,
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.get("/oapi/3/rwd/v1/getLeaderboard", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/oapi/3/rwd/v1/getLeaderboard",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.get("/accounts/auth/v1/logout", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/accounts/auth/v1/logout",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+// ncm
+app.get("/ncm/appsc/v1/getAppsList", (req, res) => {
+    axios({
+        method: 'get',
+        url: "https://now.gg/ncm/appsc/v1/getAppsList",
+    }).then(function (response) {
+        res.send(response.data);
+    })
+})
+
+app.listen(8003, () => {
+    console.log("listening fr");
+})
