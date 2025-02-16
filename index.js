273409744db37345faccee228577d13a6a8f30e2
quotingg. <112746878+quotingg@users.noreply.github.com>
2025-02-16 15:41:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const exp = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = exp();
 
-const targ = "https://neal.fun"
+const targ = "https://doctoraux.com"
 const prox = createProxyMiddleware({
     target: targ,
     changeOrigin: true,
