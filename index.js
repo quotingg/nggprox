c18ff3862779c850c1d19aa1a82fa1643457362c
quotingg. <112746878+quotingg@users.noreply.github.com>
2025-02-17 20:59:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const exp = require("express");
-const proxy = require("express-http-proxy");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const prox = require("express-http-proxy");
+//const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("node:path");
 const app = exp();
+const Cors = require("cors");
+let Express = exp;
+let Path = path;
+let App = app;
 
-const targ = "https://doctoraux.com"
+App.use("/nowgg-static", Express.static(Path.join(__dirname, "nowgg-static")));
+App.use("/apps", Express.static(Path.join(__dirname, "apps")));
+App.use("/play", Express.static(Path.join(__dirname, "play")));
+App.use("/3", Express.static(Path.join(__dirname, "3")));
+
+App.use(Cors({
+    origin: '*',
+    allowedHeaders: ['Authorization']
+}));
+
+const targ = "https://mathsspot.com"
 /*const prox = createProxyMiddleware({
     target: targ,
     changeOrigin: true,
     secure: true,
     logLevel: "debug",
     router: function (req) {
-        delete req.headers["x-real-ip"];
-        delete req.headers["x-forwarded-for"];
-        delete req.headers["x-forwarded-proto"];
-        req.headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15";
+        if (req.headers.host === new URL(targ).host) {
+            req.headers['X-Forwarded-For'] = '';
+            req.headers['X-Real-IP'] = '';
+            req.headers['Via'] = '';
+        }
         return targ;
     }
 })*/
 
-app.use("/", proxy("https://doctoraux.com"));
+app.get("/", (_Request, Response) => {
+    Response.sendFile(path.join(__dirname, "apps/rawblax", "index.html"));
+})
+app.use("/", prox('https://doctoraux.com'));
 app.listen(5000, () => {
     console.log("run");
 })
