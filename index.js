c3146dfd33779889c6f2ff62abd756595190d47e
quotingg. <112746878+quotingg@users.noreply.github.com>
2025-02-17 20:46:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const exp = require("express");
+const proxy = require("http-proxy");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = exp();
 
 const targ = "https://doctoraux.com"
-const prox = createProxyMiddleware({
+/*const prox = createProxyMiddleware({
     target: targ,
     changeOrigin: true,
     secure: true,
@@ -15,9 +16,9 @@ const prox = createProxyMiddleware({
         req.headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15";
         return targ;
     }
-})
+})*/
 
-app.use("/", prox);
+app.use("/", proxy("https://doctoraux.com"));
 app.listen(5000, () => {
     console.log("run");
 })
